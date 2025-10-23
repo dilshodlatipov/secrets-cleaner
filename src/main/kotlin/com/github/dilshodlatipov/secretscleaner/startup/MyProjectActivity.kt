bc0be4db1954bab4a85632103f3ffba178f8ac73
GitHub Action <action@github.com>
2025-10-23 05:23:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/dilshodlatipov/secretscleaner/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/dilshodlatipov/secretscleaner/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.dilshodlatipov.secretscleaner.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/dilshodlatipov/secretscleaner/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/dilshodlatipov/secretscleaner/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.dilshodlatipov.secretscleaner.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dilshodlatipov.secretscleaner.MyBundle
+import com.github.dilshodlatipov.secretscleaner.services.MyProjectService
 import javax.swing.JButton
 
 
